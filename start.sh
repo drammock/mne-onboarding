1a6cbe520273b5e0a61027c3203dc12fd1ad1e45
Daniel McCloy <dan@mccloy.info>
2025-08-01 09:52:13 -0500

    cleaner run script

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
+trap ctrl_c INT
+
+ctrl_c () {
+    kill %1  # stop the Jupyter server when done
+}
+
 PORT=8888
 export JUPYTER_BASE_URL="http://localhost:${PORT}"
 export JUPYTER_TOKEN="foo"
 jupyter server --IdentityProvider.token="${JUPYTER_TOKEN}" --ServerApp.port="${PORT}" &
-myst start --execute && echo "killing Jupyter server"
-# kill %1  # run manually, to stop the Jupyter server when done
+myst start --execute
