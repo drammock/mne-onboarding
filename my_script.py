9712b1b5d4d253f23230492155c1ca8573cafa3a
Daniel McCloy <dan@mccloy.info>
2025-08-04 12:05:06 -0500

    start with less optimized script

--- my_script.py
+++ my_script.py
@@ -1,18 +1,22 @@
 import numpy as np
 
-n = 10_000_000
+n = 100_000
 ARR = np.random.default_rng(seed=8675309).integers(10, size=(2, n))
 
 
 def my_func(input_array):
     """Compute the dot product of an array with its transpose."""
-    nrow = input_array.shape[0]
-    result = np.empty((nrow, nrow), dtype=int)
+    nrow, ncol = input_array.shape
+    result = np.zeros((nrow, nrow), dtype=int)
     for rix in range(nrow):
         for cix in range(nrow):
-            result[rix, cix] = np.sum(input_array[rix] * input_array.T[:, cix])
+            for eix in range(ncol):
+                result[rix, cix] += input_array[rix, eix] * input_array.T[eix, cix]
     return result
 
 
 if __name__ == "__main__":
-    print(my_func(ARR))
+    np.testing.assert_array_equal(
+        my_func(ARR),
+        np.array([[2842216, 2016906], [2016906, 2840381]]),
+    )
