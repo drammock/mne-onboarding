13cc504748edc06baf1aafa1e45592fe7fbabdac
Eric Larson <larson.eric.d@gmail.com>
2025-08-05 16:09:52 -0400

    ENH: More Numba (#1)

---
+++ optimization/script_numba.py
@@ -0,0 +1,24 @@
+import numpy as np
+from numba import jit  # ADDED!!!
+
+n = 100_000
+ARR = np.random.default_rng(seed=8675309).integers(10, size=(2, n))
+
+
+@jit(nopython=True)  # ADDED!!!
+def my_func(input_array):
+    """Compute the dot product of an array with its transpose."""
+    nrow, ncol = input_array.shape
+    result = np.zeros((nrow, nrow), dtype=np.int64)  # CHANGED!!!
+    for rix in range(nrow):
+        for cix in range(nrow):
+            for eix in range(ncol):
+                result[rix, cix] += input_array[rix, eix] * input_array.T[eix, cix]
+    return result
+
+
+if __name__ == "__main__":
+    np.testing.assert_array_equal(
+        my_func(ARR),
+        np.array([[2842216, 2016906], [2016906, 2840381]]),
+    )
